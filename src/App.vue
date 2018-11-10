f5a5c53d08c5302450a8dfc2def9f0ddb172f811
yangkai <393495797@qq.com>
2018-11-10 20:20:04 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,8 @@
         fullStyle: [
           `/*
 * Inspired by http://strml.net/
-* 大家好，我是方方
-* 二月了，好多公司都在招聘，你是不是也在准备简历呀。
+* 大家好，我是杨凯
+* 2018年底了，好多公司都在招聘，作为明年技术储备，你是不是也在准备简历呀。
 * 说做就做，我也来写一份简历！
 */
 
@@ -115,34 +115,34 @@ html{
 }
 `],
         currentMarkdown: '',
-        fullMarkdown: `方应杭
+        fullMarkdown: `杨凯
 ----
 
-资深前端工程师，资深前端讲师，现在在 [饥人谷](http://jirengu.com) 教前端课程。
+h5前端工程师，现在在求职哟,期待新东家。
 
 技能
 ----
 
-* 前端开发
-* Rails 开发
-* Node.js 开发
-* 前端授课
+* 框架:vue,react
+* 原生js，jquery，
+* scss css 
+* 微信小程序开发
 
 工作经历
 ----
 
-1. [饥人谷](http://jirengu.com)
-2. 腾讯即时通讯平台部
-3. 阿里巴巴B2B部门
-4. 彩程知人项目组
+1. 待更新
+2. 待更新
+3. .....
+4. ....
 
 链接
 ----
 
-* [GitHub](https://github.com/frankfang)
-* [我的文章](https://www.zhihu.com/people/zhihusucks/pins/posts)
+* [GitHub](https://github.com/yangkaiyangyi)
+* [我的文章](待更新)
 
-> 如果你喜欢这个效果，Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
+> 如果你喜欢这个效果，Fork [我的简历](https://yangkaiyangyi.github.io/resume/dist)，打造你自己的简历！
 
 `
       }
